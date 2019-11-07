38c0adcd1fd9d1ed1677b7dbeb9157bd5d0542db
KoisBota <49224548+KoisBota@users.noreply.github.com>
2019-11-07 17:39:27 +0600

    second

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ var button1 = document.getElementsByClassName('number');
 for(var i=0;i<button1.length;i++){
   button1[i].addEventListener("click",clickButton);
 }
+
+var button2 = document.getElementsByClassName('forButton');
+for(var i=0;i<button2.length;i++){
+  button2[i].addEventListener("click",clickAll);
+}
