19db4d0e68ebe3e5a690fe817aeb0506a9f807e4
пёс <49204667+psinadog@users.noreply.github.com>
2019-11-07 15:00:58 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 var button1 = document.getElementsByClassName('number');
-for(var i=0;i<button1.length;i++){
+for(var i = 0; i < button1.length; i++){
   button1[i].addEventListener("click",clickButton);
 }
 
 var button2 = document.getElementsByClassName('forButton');
-for(var i=0;i<button2.length;i++){
-  button2[i].addEventListener("click",clickAll);
+for(var i = 0; i < button2.length; i++){
+  button2[i].addEventListener("click", clickAll);
 }
-var text=document.getElementById('inp');
-var buttonEqual=document.getElementById('equal');
-var buttonSE=document.getElementById('se');
-buttonEqual.addEventListener("click",clickEqual);
-buttonSE.addEventListener("click",clickSE);
+var text = document.getElementById('inp');
+var buttonEqual = document.getElementById('equal');
+var buttonSE = document.getElementById('se');
+buttonEqual.addEventListener("click", clickEqual);
+buttonSE.addEventListener("click", clickSE);
 
 
 function clickEqual(e){
@@ -19,5 +19,5 @@ function clickEqual(e){
   var num2 = document.getElementById('inp').value;
 }
 function clickSE(e){
-    document.getElementById('inp').value = null;
+    text.value = "";
 }
